4fcf1aae875c5773c6206f8631f9975f781bcf86
Uday31-2004 <135055066+Uday31-2004@users.noreply.github.com>
2024-08-28 17:26:56 +0530

    added cors

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const connectDB = require('./config/database');
 const supplierRoutes = require('./routes/supplierRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const deliveryRoutes = require('./routes/deliveryRoutes');
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'http://localhost:5173', // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+};
+
+
 
 const app = express();
+app.use(cors(corsOptions));
 connectDB();
 
 app.use(express.json());
@@ -18,5 +28,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
 module.exports = app;
