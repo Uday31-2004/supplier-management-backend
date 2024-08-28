1f0788db4aa860472dc1d094d555d6621415194b
Uday31-2004 <135055066+Uday31-2004@users.noreply.github.com>
2024-08-28 17:42:12 +0530

    changed order-schema

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  supplier: { type: mongoose.Schema.Types.ObjectId, ref: 'Supplier', required: true },
+  supplierId: { type: mongoose.Schema.Types.ObjectId, ref: 'Supplier', required: true },
   items: [{ name: String, quantity: Number }],
   status: { type: String, default: 'Pending' },
   expectedDeliveryDate: { type: Date },
